0e93084bf8a2068986f31e7b346c3cd3b662f859
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2025-08-26 16:40:52 +0700

    feat: upload auth (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import CustomZodValidationPipe from '@/common/pipes/custom-zod-validation.pipe'
+import { HttpExceptionFilter } from '@/shared/filters/http-exception.filter'
 import { Module } from '@nestjs/common'
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import { ZodSerializerInterceptor } from 'nestjs-zod'
 import { SharedModule } from './shared/shared.module'
 
 @Module({
   imports: [SharedModule],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: CustomZodValidationPipe
+    },
+    { provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    }
+  ]
 })
 export class AppModule {}
---
+++ src/common/constants/role.constant.ts
@@ -0,0 +1,13 @@
+export const RoleName = {
+  Admin: 'ADMIN'
+} as const
+
+export const HTTPMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  PATCH: 'PATCH',
+  OPTIONS: 'OPTIONS',
+  HEAD: 'HEAD'
+} as const
